27dce38fe934a2428fdcbd8810750090f0f393cf
Ismael Aquino <ismaellAquino@hotmail.com>
2021-10-20 10:37:06 -0300

    add configuration to generate JWT

--- src/Api.Application/Startup.cs
+++ src/Api.Application/Startup.cs
@@ -1,11 +1,15 @@
+using System;
 using Api.CrossCuting.DependencyInjection;
 using Api.Domain.Security;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Options;
+using Microsoft.IdentityModel.Tokens;
 
 namespace Api
 {
@@ -28,12 +32,33 @@ namespace Api
       services.AddSingleton(signingConfiguration);
 
       TokenConfigurations tokenConfigurations = new TokenConfigurations();
-
       new ConfigureFromConfigurationOptions<TokenConfigurations>(
         Configuration.GetSection("TokenConfigurations"))
           .Configure(tokenConfigurations);
-
       services.AddSingleton(tokenConfigurations);
+
+      services.AddAuthentication(authOptions =>
+      {
+        authOptions.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+        authOptions.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+      }).AddJwtBearer(bearerOptions =>
+      {
+        TokenValidationParameters paramsValidation = bearerOptions.TokenValidationParameters;
+
+        paramsValidation.IssuerSigningKey = signingConfiguration.Key;
+        paramsValidation.ValidAudience = tokenConfigurations.Audience;
+        paramsValidation.ValidIssuer = tokenConfigurations.Issuer;
+        paramsValidation.ValidateIssuerSigningKey = true;
+        paramsValidation.ValidateLifetime = true;
+        paramsValidation.ClockSkew = TimeSpan.Zero;
+      });
+
+      services.AddAuthorization(auth =>
+      {
+        auth.AddPolicy("Bearer", new AuthorizationPolicyBuilder()
+                                .AddAuthenticationSchemes(JwtBearerDefaults.AuthenticationScheme)
+                                .RequireAuthenticatedUser().Build());
+      });
 
       services.AddControllers();
       services.AddSwaggerGen();
