b31d60b0738bbea7107a6d5191e88d33122744ea
Ismael Aquino <ismaellAquino@hotmail.com>
2021-10-21 11:54:16 -0300

    create test to verify post method

---
+++ src/Api.Service.Test/User/WhenExecutePost.cs
@@ -0,0 +1,27 @@
+using System.Threading.Tasks;
+using Api.Domain.Interfaces.Services.User;
+using Moq;
+using Xunit;
+
+namespace Api.Service.Test.User
+{
+  public class WhenExecutePost : UserTest
+  {
+    private IUserService _service;
+
+    private Mock<IUserService> _serviceMock;
+
+    [Fact(DisplayName = "When Execute Post Method")]
+    public async Task CanExecutePostMethod()
+    {
+      _serviceMock = new Mock<IUserService>();
+      _serviceMock.Setup(m => m.Post(userDtoCreate)).ReturnsAsync(userDtoCreateResult);
+      _service = _serviceMock.Object;
+
+      var _resultCreate = await _service.Post(userDtoCreate);
+      Assert.NotNull(_resultCreate);
+      Assert.Equal(UserName, _resultCreate.Name);
+      Assert.Equal(UserEmail, _resultCreate.Email);
+    }
+  }
+}
