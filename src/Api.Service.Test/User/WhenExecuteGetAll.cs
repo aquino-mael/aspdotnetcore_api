b76cfdc3719e81d81191594267e68949d008d5c7
Ismael Aquino <ismaellAquino@hotmail.com>
2021-10-21 11:29:50 -0300

    create test to verify get all method

---
+++ src/Api.Service.Test/User/WhenExecuteGetAll.cs
@@ -0,0 +1,38 @@
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Api.Domain.Dtos.User;
+using Api.Domain.Interfaces.Services.User;
+using Moq;
+using Xunit;
+
+namespace Api.Service.Test.User
+{
+  public class WhenExecuteGetAll : UserTest
+  {
+    private IUserService _service;
+
+    private Mock<IUserService> _serviceMock;
+
+    [Fact(DisplayName = "Whene execute GetAll Method")]
+    public async Task CanExecuteGetAll()
+    {
+      _serviceMock = new Mock<IUserService>();
+      _serviceMock.Setup(m => m.GetAll()).ReturnsAsync(userDtoList);
+      _service = _serviceMock.Object;
+
+      var _result = await _service.GetAll();
+      Assert.NotNull(_result);
+      Assert.True(_result.Count() == 10);
+
+      var _listResult = new List<UserDto>();
+      _serviceMock = new Mock<IUserService>();
+      _serviceMock.Setup(m => m.GetAll()).ReturnsAsync(_listResult.AsEnumerable);
+      _service = _serviceMock.Object;
+
+      var _resultEmpty = await _service.GetAll();
+      Assert.Empty(_resultEmpty);
+      Assert.True(_resultEmpty.Count() == 0);
+    }
+  }
+}
