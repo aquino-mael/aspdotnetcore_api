afc516b689901540e210469f00f0b2295027a67d
Ismael Aquino <ismaellAquino@hotmail.com>
2021-10-21 09:37:45 -0300

    create tests to verify all user crud

--- src/Api.Data.Test/UsuarioCrudComplete.cs
+++ src/Api.Data.Test/UsuarioCrudComplete.cs
@@ -30,11 +30,37 @@ namespace Api.Data.Test
           Name = Faker.Name.FullName(),
         };
 
-        var _registroCriado = await _repository.InsertAsync(_entity);
-        Assert.NotNull(_registroCriado);
-        Assert.Equal(_entity.Email, _registroCriado.Email);
-        Assert.Equal(_entity.Name, _registroCriado.Name);
-        Assert.False(_registroCriado.Id == Guid.Empty);
+        var _createRegister = await _repository.InsertAsync(_entity);
+        Assert.NotNull(_createRegister);
+        Assert.Equal(_entity.Email, _createRegister.Email);
+        Assert.Equal(_entity.Name, _createRegister.Name);
+        Assert.False(_createRegister.Id == Guid.Empty);
+
+        _entity.Name = Faker.Name.First();
+        var _registerUpdate = await _repository.UpdateAsync(_entity);
+        Assert.NotNull(_registerUpdate);
+        Assert.Equal(_entity.Email, _registerUpdate.Email);
+        Assert.Equal(_entity.Name, _registerUpdate.Name);
+
+        var _existRegister = await _repository.ExistsAsync(_registerUpdate.Id);
+        Assert.True(_existRegister);
+
+        var _selectedRegister = await _repository.SelectAsync(_registerUpdate.Id);
+        Assert.NotNull(_existRegister);
+        Assert.Equal(_registerUpdate.Email, _selectedRegister.Email);
+        Assert.Equal(_registerUpdate.Name, _selectedRegister.Name);
+
+        var _allRegisters = await _repository.SelectAsync();
+        Assert.NotNull(_allRegisters);
+        Assert.NotEmpty(_allRegisters);
+
+        var _isRemovedRegister = await _repository.DeleteAsync(_selectedRegister.Id);
+        Assert.True(_isRemovedRegister);
+
+        var _defaultUser = await _repository.FindByLogin("admin@hotmail.com");
+        Assert.NotNull(_defaultUser);
+        Assert.Equal("admin@hotmail.com", _defaultUser.Email);
+        Assert.Equal("Administrador", _defaultUser.Name);
       }
     }
   }
