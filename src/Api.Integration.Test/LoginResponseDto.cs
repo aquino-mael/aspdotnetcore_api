88222a3e965749fe6d38eb3b36fb435d9c646d59
Ismael Aquino <ismaellAquino@hotmail.com>
2021-10-22 11:30:02 -0300

    create login response DTO

---
+++ src/Api.Integration.Test/LoginResponseDto.cs
@@ -0,0 +1,24 @@
+namespace Api.Integration.Test
+{
+  public class LoginResponseDto
+  {
+
+    [JsonProperty("authenticated")]
+    public bool authenticated { get; set; }
+
+    [JsonProperty("created")]
+    public DateTime created { get; set; }
+
+    [JsonProperty("expiration")]
+    public DateTime expiration { get; set; }
+
+    [JsonProperty("accessToken")]
+    public string accessToken { get; set; }
+
+    [JsonProperty("username")]
+    public string username { get; set; }
+
+    [JsonProperty("message")]
+    public string message { get; set; }
+  }
+}
