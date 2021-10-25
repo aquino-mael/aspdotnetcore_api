ea709fce90117ffdb8dfcc82290e5b2ab69563ba
Ismael Aquino <ismaellAquino@hotmail.com>
2021-10-25 14:09:10 -0300

    create uf entity

---
+++ src/Api.Domain/Entities/UfEntity.cs
@@ -0,0 +1,16 @@
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
+namespace Api.Domain.Entities
+{
+  public class UfEntity : BaseEntity
+  {
+    [Required]
+    [MaxLength(2)]
+    public string Initials { get; set; }
+    [Required]
+    [MaxLength(45)]
+    public string Name { get; set; }
+    public IEnumerable<CountyEntity> Counties { get; set; }
+  }
+}
