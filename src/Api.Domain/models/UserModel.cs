74dd964739e680a829649f8468815b13c2a696a5
Ismael Aquino <ismaellAquino@hotmail.com>
2021-10-25 14:49:12 -0300

    create base model

--- src/Api.Domain/models/UserModel.cs
+++ src/Api.Domain/models/UserModel.cs
@@ -2,15 +2,8 @@ using System;
 
 namespace Api.Domain.models
 {
-  public class UserModel
+  public class UserModel : BaseModel
   {
-    private Guid _id;
-    public Guid Id
-    {
-      get { return _id; }
-      set { _id = value; }
-    }
-
     private string _name;
     public string Name
     {
@@ -23,23 +16,6 @@ namespace Api.Domain.models
     {
       get { return _email; }
       set { _email = value; }
-    }
-
-    private DateTime _createdAt;
-    public DateTime CreatedAt
-    {
-      get { return _createdAt; }
-      set
-      {
-        _createdAt = value == null ? DateTime.UtcNow : value;
-      }
-    }
-
-    private DateTime _updatedAt;
-    public DateTime UpdatedAt
-    {
-      get { return _updatedAt; }
-      set { _updatedAt = value; }
     }
   }
 }
