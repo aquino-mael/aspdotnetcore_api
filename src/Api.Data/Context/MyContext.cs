0f510b0be318609ddefa3e358d6834f2908fe242
Ismael Aquino <ismaellAquino@hotmail.com>
2021-10-25 15:51:02 -0300

    apply mappers on the context

--- src/Api.Data/Context/MyContext.cs
+++ src/Api.Data/Context/MyContext.cs
@@ -2,27 +2,34 @@ using Microsoft.EntityFrameworkCore;
 using Api.Domain.Entities;
 using Api.Data.Mapping;
 using System;
+using Api.Data.Seeds;
 
 namespace Api.Data.Context
 {
-  public class MyContext : DbContext
-  {
-    public MyContext(DbContextOptions<MyContext> options) : base(options) { }
-
-    public DbSet<UserEntity> Users { get; set; }
-
-    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    public class MyContext : DbContext
     {
-      base.OnModelCreating(modelBuilder);
+        public MyContext(DbContextOptions<MyContext> options) : base(options) { }
 
-      modelBuilder.Entity<UserEntity>(new UserMap().Configure);
-      modelBuilder.Entity<UserEntity>().HasData(new UserEntity
-      {
-        Id = Guid.NewGuid(),
-        Email = "admin@hotmail.com",
-        Name = "Administrador",
-        CreatedAt = DateTime.UtcNow,
-      });
+        public DbSet<UserEntity> Users { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            modelBuilder.Entity<UserEntity>(new UserMap().Configure);
+            modelBuilder.Entity<UfEntity>(new UfMap().Configure);
+            modelBuilder.Entity<ZipCodeEntity>(new ZipCodeMap().Configure);
+            modelBuilder.Entity<CountyEntity>(new CountyMap().Configure);
+
+            modelBuilder.Entity<UserEntity>().HasData(new UserEntity
+            {
+                Id = Guid.NewGuid(),
+                Email = "admin@hotmail.com",
+                Name = "Administrador",
+                CreatedAt = DateTime.UtcNow,
+            });
+
+            UfSeeds.Ufs(modelBuilder);
+        }
     }
-  }
 }
