96e5792cac0c88595951e2441d84160399eff22f
Ismael Aquino <ismaellAquino@hotmail.com>
2021-10-29 10:10:30 -0300

    migrate project to .NET 5.0

--- src/Api.Application/Startup.cs
+++ src/Api.Application/Startup.cs
@@ -19,103 +19,103 @@ using Microsoft.OpenApi.Models;
 
 namespace Api
 {
-  public class Startup
-  {
-    public Startup(IConfiguration configuration, IWebHostEnvironment environment)
+    public class Startup
     {
-      Configuration = configuration;
-      _environment = environment;
-    }
+        public Startup(IConfiguration configuration, IWebHostEnvironment environment)
+        {
+            Configuration = configuration;
+            _environment = environment;
+        }
 
-    public IConfiguration Configuration { get; }
-    public IWebHostEnvironment _environment { get; }
+        public IConfiguration Configuration { get; }
+        public IWebHostEnvironment _environment { get; }
 
-    // This method gets called by the runtime. Use this method to add services to the container.
-    public void ConfigureServices(IServiceCollection services)
-    {
-      if (_environment.IsEnvironment("Testing"))
-      {
-        Environment.SetEnvironmentVariable(
-          "DB_CONNECTION",
-          @"
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            if (_environment.IsEnvironment("Testing"))
+            {
+                Environment.SetEnvironmentVariable(
+                  "DB_CONNECTION",
+                  @"
           Server = localhost;
           Initial Catalog = dbapi_Integration;
           MultipleActiveResultSets = true;
           User ID = sa;
           Password = DockerSql2017!"
-        );
-        Environment.SetEnvironmentVariable("DATABASE", "SQLSERVER");
-        Environment.SetEnvironmentVariable("MIGRATION", "APPLY");
-        Environment.SetEnvironmentVariable("AUDIENCE", "ExempleAudience");
-        Environment.SetEnvironmentVariable("ISSUER", "ExempleIssuer");
-        Environment.SetEnvironmentVariable("TOKEN_SECONDS", "28800");
-      }
+                );
+                Environment.SetEnvironmentVariable("DATABASE", "SQLSERVER");
+                Environment.SetEnvironmentVariable("MIGRATION", "APPLY");
+                Environment.SetEnvironmentVariable("AUDIENCE", "ExempleAudience");
+                Environment.SetEnvironmentVariable("ISSUER", "ExempleIssuer");
+                Environment.SetEnvironmentVariable("TOKEN_SECONDS", "28800");
+            }
 
-      services.AddControllers();
+            services.AddControllers();
 
-      ConfigureService.ConfigureDependenciesService(services);
-      ConfigureRepository.ConfigureDependenciesRepository(services);
+            ConfigureService.ConfigureDependenciesService(services);
+            ConfigureRepository.ConfigureDependenciesRepository(services);
 
-      var config = new AutoMapper.MapperConfiguration(config =>
-      {
-        config.AddProfile(new DtoToModelProfile());
-        config.AddProfile(new EntityToDtoProfile());
-        config.AddProfile(new ModelToEntityProfile());
-      });
+            var config = new AutoMapper.MapperConfiguration(config =>
+            {
+                config.AddProfile(new DtoToModelProfile());
+                config.AddProfile(new EntityToDtoProfile());
+                config.AddProfile(new ModelToEntityProfile());
+            });
 
-      IMapper mapper = config.CreateMapper();
-      services.AddSingleton(mapper);
+            IMapper mapper = config.CreateMapper();
+            services.AddSingleton(mapper);
 
-      SigningConfigurations signingConfiguration = new SigningConfigurations();
-      services.AddSingleton(signingConfiguration);
+            SigningConfigurations signingConfiguration = new SigningConfigurations();
+            services.AddSingleton(signingConfiguration);
 
-      services.AddAuthentication(authOptions =>
-      {
-        authOptions.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
-        authOptions.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
-      }).AddJwtBearer(bearerOptions =>
-      {
-        TokenValidationParameters paramsValidation = bearerOptions.TokenValidationParameters;
+            services.AddAuthentication(authOptions =>
+            {
+                authOptions.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+                authOptions.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+            }).AddJwtBearer(bearerOptions =>
+            {
+                TokenValidationParameters paramsValidation = bearerOptions.TokenValidationParameters;
 
-        paramsValidation.IssuerSigningKey = signingConfiguration.Key;
-        paramsValidation.ValidAudience = Environment.GetEnvironmentVariable("AUDIENCE");
-        paramsValidation.ValidIssuer = Environment.GetEnvironmentVariable("ISSUER");
-        paramsValidation.ValidateIssuerSigningKey = true;
-        paramsValidation.ValidateLifetime = true;
-        paramsValidation.ClockSkew = TimeSpan.Zero;
-      });
+                paramsValidation.IssuerSigningKey = signingConfiguration.Key;
+                paramsValidation.ValidAudience = Environment.GetEnvironmentVariable("AUDIENCE");
+                paramsValidation.ValidIssuer = Environment.GetEnvironmentVariable("ISSUER");
+                paramsValidation.ValidateIssuerSigningKey = true;
+                paramsValidation.ValidateLifetime = true;
+                paramsValidation.ClockSkew = TimeSpan.Zero;
+            });
 
-      services.AddAuthorization(auth =>
-      {
-        auth.AddPolicy("Bearer", new AuthorizationPolicyBuilder()
-                                .AddAuthenticationSchemes(JwtBearerDefaults.AuthenticationScheme)
-                                .RequireAuthenticatedUser().Build());
-      });
+            services.AddAuthorization(auth =>
+            {
+                auth.AddPolicy("Bearer", new AuthorizationPolicyBuilder()
+                                  .AddAuthenticationSchemes(JwtBearerDefaults.AuthenticationScheme)
+                                  .RequireAuthenticatedUser().Build());
+            });
 
-      services.AddSwaggerGen(swaggerGenOptions =>
-      {
-        swaggerGenOptions.SwaggerDoc("v1", new OpenApiInfo
-        {
-          Version = "v1",
-          Title = "Curso de API com AspNetCore 3.1 - Na Prática",
-          Description = "Arquitetura DDD",
-          TermsOfService = new Uri("http://mfrinfo.com.br"),
-          Contact = new OpenApiContact
-          {
-            Name = "Ismael Aquino",
-            Email = "ismaellAquino@hotmail.com",
-          },
-        });
+            services.AddSwaggerGen(swaggerGenOptions =>
+            {
+                swaggerGenOptions.SwaggerDoc("v1", new OpenApiInfo
+                {
+                    Version = "v1",
+                    Title = "Curso de API com AspNetCore 3.1/.Net5.0 - Na Prática",
+                    Description = "Arquitetura DDD",
+                    TermsOfService = new Uri("http://mfrinfo.com.br"),
+                    Contact = new OpenApiContact
+                    {
+                        Name = "Ismael Aquino",
+                        Email = "ismaellAquino@hotmail.com",
+                    },
+                });
 
-        swaggerGenOptions.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme
-        {
-          Description = "Gerar JWT",
-          Name = "Authorization",
-          In = ParameterLocation.Header,
-          Type = SecuritySchemeType.ApiKey,
-        });
+                swaggerGenOptions.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme
+                {
+                    Description = "Gerar JWT",
+                    Name = "Authorization",
+                    In = ParameterLocation.Header,
+                    Type = SecuritySchemeType.ApiKey,
+                });
 
-        swaggerGenOptions.AddSecurityRequirement(new OpenApiSecurityRequirement {
+                swaggerGenOptions.AddSecurityRequirement(new OpenApiSecurityRequirement {
           {
             new OpenApiSecurityScheme {
               Reference = new OpenApiReference {
@@ -124,49 +124,49 @@ namespace Api
               }
             }, new List<string>()
           }
-        });
-      });
-    }
-
-    // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-    public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
-    {
-      if (env.IsDevelopment())
-      {
-        app.UseDeveloperExceptionPage();
-      }
-      app.UseSwagger();
-
-      app.UseSwaggerUI(
-        swaggerUIOptions =>
-        {
-          swaggerUIOptions.SwaggerEndpoint(
-            "/swagger/v1/swagger.json",
-            "Curso de API com AspNetCore 3.1"
-          );
-          swaggerUIOptions.RoutePrefix = string.Empty;
+              });
+            });
         }
-      );
 
-      app.UseRouting();
-
-      app.UseAuthorization();
-
-      app.UseEndpoints(endpoints =>
-      {
-        endpoints.MapControllers();
-      });
-
-      if (Environment.GetEnvironmentVariable("MIGRATION").ToLower() == "APPLY".ToLower())
-      {
-        using (var service = app.ApplicationServices.GetRequiredService<IServiceScopeFactory>().CreateScope())
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
-          using (var context = service.ServiceProvider.GetService<MyContext>())
-          {
-            context.Database.Migrate();
-          }
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+            app.UseSwagger();
+
+            app.UseSwaggerUI(
+              swaggerUIOptions =>
+              {
+                  swaggerUIOptions.SwaggerEndpoint(
+              "/swagger/v1/swagger.json",
+              "Curso de API com AspNetCore 3.1/.Net5.0 - Na Prática"
+            );
+                  swaggerUIOptions.RoutePrefix = string.Empty;
+              }
+            );
+
+            app.UseRouting();
+
+            app.UseAuthorization();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllers();
+            });
+
+            if (Environment.GetEnvironmentVariable("MIGRATION").ToLower() == "APPLY".ToLower())
+            {
+                using (var service = app.ApplicationServices.GetRequiredService<IServiceScopeFactory>().CreateScope())
+                {
+                    using (var context = service.ServiceProvider.GetService<MyContext>())
+                    {
+                        context.Database.Migrate();
+                    }
+                }
+            }
         }
-      }
     }
-  }
 }
--- src/Api.CrossCuting/DependencyInjection/ConfigureRepository.cs
+++ src/Api.CrossCuting/DependencyInjection/ConfigureRepository.cs
@@ -6,31 +6,38 @@ using Api.Domain.Interfaces;
 using Api.Domain.Repository;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.DependencyInjection;
+using Pomelo.EntityFrameworkCore.MySql.Infrastructure;
 
 namespace Api.CrossCuting.DependencyInjection
 {
-  public class ConfigureRepository
-  {
-    public static void ConfigureDependenciesRepository(IServiceCollection serviceCollection)
+    public class ConfigureRepository
     {
-      serviceCollection.AddScoped(typeof(IRepository<>), typeof(BaseRepository<>));
-      serviceCollection.AddScoped<IUserRepository, UserImplementation>();
-      serviceCollection.AddScoped<IUfRepository, UfImplementation>();
-      serviceCollection.AddScoped<ICountyRepository, CountyImplementation>();
-      serviceCollection.AddScoped<IZipCodeRepository, ZipCodeImplementation>();
+        public static void ConfigureDependenciesRepository(IServiceCollection serviceCollection)
+        {
+            serviceCollection.AddScoped(typeof(IRepository<>), typeof(BaseRepository<>));
+            serviceCollection.AddScoped<IUserRepository, UserImplementation>();
+            serviceCollection.AddScoped<IUfRepository, UfImplementation>();
+            serviceCollection.AddScoped<ICountyRepository, CountyImplementation>();
+            serviceCollection.AddScoped<IZipCodeRepository, ZipCodeImplementation>();
 
-      if (Environment.GetEnvironmentVariable("DATABASE").ToLower() == "SQLSERVER".ToLower())
-      {
-        serviceCollection.AddDbContext<MyContext>(
-          options => options.UseSqlServer(Environment.GetEnvironmentVariable("DB_CONNECTION"))
-        );
-      }
-      else
-      {
-        serviceCollection.AddDbContext<MyContext>(
-          options => options.UseMySql(Environment.GetEnvironmentVariable("DB_CONNECTION"))
-        );
-      }
+            if (Environment.GetEnvironmentVariable("DATABASE").ToLower() == "SQLSERVER".ToLower())
+            {
+                serviceCollection.AddDbContext<MyContext>(
+                  options => options.UseSqlServer(Environment.GetEnvironmentVariable("DB_CONNECTION"))
+                );
+            }
+            else
+            {
+                serviceCollection.AddDbContext<MyContext>(
+                  options => options.UseMySql(
+                    Environment.GetEnvironmentVariable("DB_CONNECTION"),
+                    new MySqlServerVersion(
+                      new Version(8, 0, 21)
+                    ),
+                    mySqlOption => mySqlOption.CharSetBehavior(CharSetBehavior.NeverAppend)
+                  )
+                );
+            }
+        }
     }
-  }
 }
